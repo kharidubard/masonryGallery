97b8c056f8e81cb193d32ba96dd9b329364c236f
kharidubard <kharidubard@gmail.com>
2025-02-01 16:35:34 -0500

    masonryGalleryComponent

---
+++ masonryGalleryComponent/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">Masonry Gallery</h1>
+    </div>
+  );
+};
+
+export default App;
+
